954ed0adb5360287159c3c1282a5080919aee96c
dlgldgld <shin12272014@gmail.com>
2022-04-23 23:09:00 +0900

    BOJ_4963 clear

--- BOJ/BFS/BOJ_4963.py
+++ BOJ/BFS/BOJ_4963.py
@@ -1,7 +1,9 @@
-from tkinter import N
+import sys
+
+sys.setrecursionlimit(10**6)
 
 in_map = [[0] * 50 for _ in range(50)]
-visited = [[0] * 50 for _ in range(50)]
+visited = []
 
 X_DIR = [-1, -1, 0, 1, 1, 1, 0, -1]
 Y_DIR = [0, 1, 1, 1, 0, -1, -1, -1]
@@ -10,21 +12,42 @@ w, h = 0, 0
 
 
 def tracking(x, y):
-    if x == -1 or y == -1 or x == w or y == h:
-        return
-
     if visited[x][y] == True:
-        return
+        return False
+
+    visited[x][y] = True
 
     for i in range(8):
-        tracking(x + X_DIR[i], y + Y_DIR[i])
+        n_x = x + X_DIR[i]
+        n_y = y + Y_DIR[i]
+
+        if n_x == -1 or n_y == -1 or n_x == w or n_y == h:
+            continue
+
+        if visited[n_x][n_y] == True:
+            continue
+
+        if in_map[n_x][n_y] != 0:
+            tracking(n_x, n_y)
+
+    return True
 
 
 while True:
+    visited = [[0] * 50 for _ in range(50)]
     w, h = map(int, input().split(" "))
     if w == 0 and h == 0:
-        exit(0)
+        break
 
+    for i in range(h):
+        a = map(int, input().split(" "))
+        for j, val in enumerate(a):
+            in_map[j][i] = val
+
+    answer = 0
     for i in range(w):
         for j in range(h):
-            in_map[i][j] = input()
+            if in_map[i][j] != 0 and True == tracking(i, j):
+                answer += 1
+
+    print(answer)
