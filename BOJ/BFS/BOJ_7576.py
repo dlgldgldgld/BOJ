8a37ed0f70cd1229c1cd3a98b87ad2a13b9bfec9
dlgldgld <shin12272014@gmail.com>
2022-04-25 10:06:55 +0900

    boj_7576 update

--- BOJ/BFS/BOJ_7576.py
+++ BOJ/BFS/BOJ_7576.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from collections import deque
 from sys import stdin
 
 class st_task:
@@ -7,55 +8,71 @@ class st_task:
         self.y = y
         self.dis = dis
 
-f = open("test.txt", "r")
-# w, h = map(int, input().split(' '))
-# w, h = map(int, stdin.readline().split(' '))
-w, h = map(int, f.readline().split(' '))
-board = []
-visited = []
-for _ in range(h) :
-    board.append(list(map(int, f.readline().split(' '))))
-    # board.append(list(map(int, stdin.readline().split(' '))))
-    # board.append(list(map(int, input().split(' '))))
-    visited.append([False] * w)
+    def __str__(self):
+        return f'x={self.x}, y={self.y}, dis={self.dis}'
 
-d_x = [-1, 0, 1, 0]
-d_y = [0, 1, 0, -1]
+def main():
+    # f = open("test.txt", "r")
+    # w, h = map(int, input().split(' '))
+    w, h = map(int, stdin.readline().split(' '))
+    # w, h = map(int, f.readline().split(' '))
+    board = []
+    visited = []
+    for _ in range(h) :
+        # board.append(list(map(int, f.readline().split(' '))))
+        board.append(list(map(int, stdin.readline().split(' '))))
+        # board.append(list(map(int, input().split(' '))))
+        visited.append([False] * w)
 
-queue = Queue()
-for y in range(h):
-        for x in range(w):
-            if board[y][x] == 1 :
-                visited[y][x] == True
-                queue.put(st_task(x,y,0))
+    d_x = [-1, 0, 1, 0]
+    d_y = [0, 1, 0, -1]
 
+    answer = -1
+    call = 0
 
-
-answer = -1
-call = 0
-while not queue.empty():
-    task = queue.get()
-    c_x = task.x
-    c_y = task.y
-    c_dis = task.dis
-    answer = max(answer, c_dis)
-    
-    for i in range(4):
-        call += 1
-        n_x = c_x + d_x[i]
-        n_y = c_y + d_y[i]
-        if n_x == -1 or n_y == -1 or n_x == w or n_y == h :
-            continue
-        if visited[n_y][n_x] == True or board[n_y][n_x] != 0:
-            continue
-        n_dis = c_dis + 1
-        visited[n_y][n_x] = True
-        queue.put(st_task(n_x,n_y,n_dis))
-
-for x in range(w):
+    queue = deque()
     for y in range(h):
-        if board[y][x] == 0 and visited[y][x] == False:
-            answer = -1
+            for x in range(w):
+                if board[y][x] == 1 :
+                    visited[y][x] == True
+                    queue.append(st_task(x,y,0))
 
-print(answer)
-#print(call)
+    while queue:
+        task = queue.popleft()
+        c_x = task.x
+        c_y = task.y
+        c_dis = task.dis
+        # print(task)
+        answer = max(answer, c_dis)
+        call += 1
+        for i in range(4):
+            n_x = c_x + d_x[i]
+            n_y = c_y + d_y[i]
+            if n_x == -1 or n_y == -1 or n_x == w or n_y == h:
+                continue
+            if visited[n_y][n_x] == True or board[n_y][n_x] != 0:
+                continue
+            
+            visited[n_y][n_x] = True
+            queue.append(st_task(n_x,n_y,c_dis + 1))
+
+    for x in range(w):
+        for y in range(h):
+            if board[y][x] == 0 and visited[y][x] == False:
+                answer = -1
+
+    print(answer)
+    # print(call)
+
+if __name__ == "__main__":
+    main()
+    # from cProfile import Profile
+    # from pstats import Stats
+
+    # profiler = Profile()
+    # profiler.runcall(main)
+
+    # stats = Stats(profiler)
+    # stats.strip_dirs()
+    # stats.sort_stats('cumulative')
+    # stats.print_stats()
