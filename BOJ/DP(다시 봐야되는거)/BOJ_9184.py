ed7783debb6ebb6a22881cd71deb075573175b9c
God Hansoo <shin12272014@gmail.com>
2022-05-01 23:47:33 +0900

    Create BOJ_9184.py

---
+++ BOJ/DP(다시 봐야되는거)/BOJ_9184.py
@@ -0,0 +1,34 @@
+
+dp = []
+for i in range(21):
+    dp.append([])
+    for j in range(21):
+        dp[i].append([])
+        for k in range(21):
+            dp[i][j].append(1)
+
+
+for i in range(1, 21):
+    for j in range(1, 21):
+        for k in range(1, 21):
+            if i< j and j < k:
+                dp[i][j][k] = dp[i][j][k-1] + dp[i][j-1][k-1] - dp[i][j-1][k]
+            else :
+                dp[i][j][k] = dp[i-1][j][k] + dp[i-1][j-1][k] + dp[i-1][j][k-1] - dp[i-1][j-1][k-1]
+
+while (True):
+    i,j,k = map(int, input().split())
+    if i == -1 and j == -1 and k == -1:
+        break
+    
+    answer = 1
+    if i <= 0 or j <= 0 or k <= 0 :
+        answer = 1
+    elif i > 20 or j > 20 or k > 20:
+        answer = dp[20][20][20]
+    else:
+        answer = dp[i][j][k]
+
+    print(f"w({i}, {j}, {k}) = {answer}")
+
+    
