472796d69a5b4e38f9b6ea7eb9c535ef999c2a6e
dlgldgld <shin12272014@gmail.com>
2022-05-05 13:53:49 +0900

    boj 1072 clear

--- BOJ/이분탐색/BOJ_1072.py
+++ BOJ/이분탐색/BOJ_1072.py
@@ -1,15 +1,17 @@
+from decimal import Decimal
+
 x, y = map(int, input().split())
-if x == y or 99 == int ( y / x * 100 ):
+if x == y or 99 == int ( Decimal(y) / Decimal(x) * 100 ):
     print(-1)
 else :
-    base = int ( y / x * 100 )
-    r = 4000000000
+    base = int ( Decimal(y) / Decimal(x) * 100 )
+    r = 1000000000
     l = 0
     ex_r = -1
     ex_l = -1
     while True :
-        middle = int((r+l)/2)
-        nodap = int(( y + middle ) / ( x + middle ) * 100 )
+        middle = int((Decimal(r)+Decimal(l))/2)
+        nodap = int(Decimal(y+middle) / Decimal(x+middle) * 100 )
         
         if base < nodap :
             r = middle
@@ -20,8 +22,10 @@ else :
         ex_r = r
         ex_l = l
     
-    if base != int((y+l)/(x+l)*100):
+    if base != int(Decimal(y+l)/Decimal(x+l)*100):
         print(l)
-    else :
+    elif base != int(Decimal(y+r)/Decimal(x+r)*100) :
         print(r)
+    else :
+        print(-1)
     
