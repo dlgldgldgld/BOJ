899ccf8af3a79570b7b16710e7bed0d02fa69c73
dlgldgld <shin12272014@gmail.com>
2022-05-06 16:41:09 +0900

    표편집 clear

--- PROGRAMMERS/2021_카카오_채용연계형_인턴십/03_표편집.py
+++ PROGRAMMERS/2021_카카오_채용연계형_인턴십/03_표편집.py
@@ -1,42 +1,43 @@
-from bisect import bisect_left, insort
 from collections import deque
+from heapq import heappush, heappop, heapify
 
 def solution(n, k, cmds):
     cursor = k
-    store = [i for i in range(n)]
+    left_cursor = [ -i for i in range(0, k) ]
+    right_cursor = [ i for i in range(k+1, n)]
+    heapify(left_cursor)
+    heapify(right_cursor)
     dellist = deque()
     
     for cmd in cmds :
         if cmd[0] == "U" or cmd[0] == "D":
             c, d = cmd.split()[0], int(cmd.split()[1])
             if c == "U":
-                cursor -= d
+                for _ in range(d):
+                    heappush(right_cursor, cursor)
+                    cursor = -heappop(left_cursor)
             elif c =="D" :
-                cursor += d
+                for _ in range(d):
+                    heappush(left_cursor, -cursor)
+                    cursor = heappop(right_cursor)
         else:
             if cmd[0] == "C":
-                # 삭제 연산
-                f_idx = bisect_left(store, store[cursor])
-                dellist.append(store[f_idx])
-                if f_idx == len(store) - 1:
-                    cursor -= 1
-                del store[f_idx]
-                pass
+                dellist.append(cursor)
+                if right_cursor:
+                    cursor = heappop(right_cursor)
+                else :
+                    cursor = -heappop(left_cursor)
             else :
-                # 'Z' 복구 작업
                 recover = dellist.pop()
-                f_idx = bisect_left(store, recover)
-                if cursor >= f_idx :
-                    cursor += 1
-                insort(store, recover)
+                if recover < cursor :
+                    heappush(left_cursor, -recover)
+                else :
+                    heappush(right_cursor, recover)
 
-    answer = ['X' for _ in range(n)]
-    for val in store:
-        answer[val] = 'O'
+    answer = ['O' for _ in range(n)]
+    for val in dellist:
+        answer[val] = 'X'
     
     return "".join(answer)
 
-def main():
-    solution(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"])
-
-main()
+print(solution(8, 0, ["D 7", "C", "U 6", "C", "Z", "Z", "C"]))
