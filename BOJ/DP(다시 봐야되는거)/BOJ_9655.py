b5546f8af4b3c6d36c8de3595b0a237cd7469577
dlgldgld <shin12272014@gmail.com>
2022-05-10 01:11:24 +0900

    수정

--- BOJ/DP(다시 봐야되는거)/BOJ_9655.py
+++ BOJ/DP(다시 봐야되는거)/BOJ_9655.py
@@ -1,14 +1,6 @@
 dp = [1] * 1001
-dp[1] = 1
-
+dp[2] = 0
 for i in range(2, 1001):
-    if i == 2 :
-        dp[i] = 0
-        continue
-    elif i == 3 :
-        dp[i] = 1
-        continue
-
     if dp[i-1] == 1 and dp[i-3] == 1:
         dp[i] = 0
 
