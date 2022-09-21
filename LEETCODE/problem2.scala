9ec1a6247fbd8b3f2a56205aeceab9da7c938914
God Hansoo <shin12272014@gmail.com>
2022-09-22 01:17:46 +0900

    Update problem2.scala

--- LEETCODE/problem2.scala
+++ LEETCODE/problem2.scala
@@ -1,23 +1,37 @@
 object problem2 {
+  def ListNodeToList( l1 : ListNode ) : List[Int] = {
+    var res : List[Int] = List()
+    var t_l1 = l1
+    while( t_l1 != null ){
+      res = res :+ t_l1.x
+      t_l1 = t_l1.next
+    }
+    res
+  }
+
 
   def addTwoNumbers(l1: ListNode, l2: ListNode): Unit = {
-    var n1 = l1
-    var n2 = l2
-    var ans = new ListNode(0)
-    var curr = ans
-    while(n1.next == null && n2.next == null){
-      if (n1.next != null) {
-        n1 = n1.next
-      } else {
-        n1 = new ListNode(0, null)
-      }
+    val ex1 = l1
+    val ex2 = l2
+    val l1_list = ListNodeToList(ex1)
+    val l2_list = ListNodeToList(ex2)
+    val zip_all = l1_list.zipAll(l2_list,0 ,0)
+    val zip_all_map = zip_all.map(a => a._1 + a._2)
 
-      if (n2.next != null) {
-        n2 = n2.next
-      } else {
-        n2 = new ListNode(0, null)
+    var res : ListNode = new ListNode(0, null)
+    val head = res
+    var next = 0
+    zip_all_map.zipWithIndex.foreach( a => {
+      res.x = (a._1 + next) % 10
+      if ( ( a._1 + next ) >= 10 ) next = 1
+      else next = 0
+      if (zip_all_map.length - 1 > a._2) {
+        res.next = new ListNode(-1, null)
+        res = res.next
       }
-
-    }
+    })
+    if ( next == 1 ) res.next = new ListNode(1, null)
+    else res = null
+    head
   }
 }
