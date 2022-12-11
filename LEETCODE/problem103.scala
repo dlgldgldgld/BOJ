5298f3bcc849ee16c93c37f591b432ed1d7e5939
God Hansoo <shin12272014@gmail.com>
2022-12-11 11:13:17 +0000

    modified.

--- LEETCODE/problem103.scala
+++ LEETCODE/problem103.scala
@@ -1,17 +1,16 @@
 object problem103 extends App{
   def zigzag(lv:Int, list:List[TreeNode]): List[TreeNode] = {
     lv % 2 match {
-      case 1 => list
+      case 0 => list
       case _ => list.reverse
     }
   }
 
   def zigzagLevelOrder(root: TreeNode): List[List[Int]] = {
-    var lv = 0
     LazyList.iterate(Option(root).toList){
       queue => queue.flatMap(node => Option(node.left) ++ Option(node.right))
-    }.takeWhile(x => x.nonEmpty).map(
-      x => {lv += 1; zigzag(lv, x);}.map(_.value)).toList
+    }.takeWhile(x => x.nonEmpty).zipWithIndex.map(
+      x => {zigzag(x._2, x._1)}.map(_.value)).toList
   }
 
   val l = new TreeNode(2, new TreeNode(4,null,null), null)
